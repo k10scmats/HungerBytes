4aa47fb2699c0cac17626626409af22bc5afb32c
Frank <frankliaoxy@berkeley.edu>
2018-10-20 09:53:48 -0700

    Frank Merge views.py

--- HBSite/maxkiosk/views.py
+++ HBSite/maxkiosk/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import User
 
 # Create your views here.
 
@@ -8,8 +9,11 @@ def get_button_list(box=(860, 420, 1130, 640), n_buttons = 6, bgr=0.8, height=60
     return zip(link_list, coord_list)
 
 def index(request):
-    context = { 
+    # latest_user = User.object.order_by('-pub_date')[:5]
+
+    context = {
         "name" : "Jon",
-        "button_list" : get_button_list(box=(860, 420, 1130, 640), n_buttons = 6, bgr=0.8, height=60)
+        "button_list" : get_button_list(box=(860, 420, 1130, 640), n_buttons = 6, bgr=0.8, height=60),
+        # "user list" : latest_user
     }
     return render(request, 'maxkiosk/index.html', context=context)
