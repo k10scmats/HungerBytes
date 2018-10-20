697793d1f3b2f36bf6044cd94f2a104491f4bf68
Wallace <wallacj@americas.corpdir.net>
2018-10-20 06:31:59 -0700

    skeley

--- HBSite/maxkiosk/views.py
+++ HBSite/maxkiosk/views.py
@@ -3,5 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+def get_button_list(box=(860, 420, 1130, 640), n_buttons = 6, bgr=0.8, height=60, link_list=["page.html"]):
+    coord_list = [[0,0,0,0]]
+    return zip(link_list, coord_list)
+
 def index(request):
-    return render(request, 'maxkiosk/index.html')
+    context = { 
+        "name" : "Jon",
+        "button_list" : get_button_list(box=(860, 420, 1130, 640), n_buttons = 6, bgr=0.8, height=60)
+    }
+    return render(request, 'maxkiosk/index.html', context=context)
