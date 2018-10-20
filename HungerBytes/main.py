0b323275776409eb87dc8b25ff52d20242f2db75
k10scmats <k10scmats@gmail.com>
2018-10-20 01:06:32 -0700

    Screen Transition

--- HungerBytes/main.py
+++ HungerBytes/main.py
@@ -1,10 +1,5 @@
-from kivy.uix.image import Image
-from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
 from kivy.app import App
-from functools import partial
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.properties import NumericProperty
 from kivy.lang import Builder
 
 
@@ -13,33 +8,25 @@ Builder.load_string('''
 #:import SlideTransition kivy.uix.screenmanager.SlideTransition
 
 <CustomScreen>:
+    Image:
+        source:'Main_Background.jpg'
+        
     Button:
-        text: 'B1'
+        text: 'Back'
+        size_hint: None, None
+        size: 150, 50
+        on_press:   root.manager.transition = \
+                    SlideTransition(direction="right")
+        on_release: root.manager.current = root.manager.previous()
+        
+    Button:
+        text: 'Next'
         size_hint: None, None
         pos_hint: {'right': 1}
         size: 150, 50
         on_press:   root.manager.transition = \
                     SlideTransition(direction="left")
         on_release: root.manager.current = root.manager.next()
-
-    Button:
-        text: 'B2'
-        size_hint: None, None
-        size: 150, 50
-        on_press:   root.manager.transition = \
-                    SlideTransition(direction="right")
-        on_release: root.manager.current = root.manager.previous()
-
-    BoxLayout:
-        size_hint: .5, None
-        height: 250
-        pos_hint: {'center_x': .5}
-        orientation: 'vertical'
-
-        Button:
-            text: 'B3'
-            on_release: root.manager.transition = \
-                        SlideTransition(direction="left")
 ''')
 
 
